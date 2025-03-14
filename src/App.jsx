132d2a5388c97d9cc9caf07fe5557b653e4d1179
Govind Singh <110704907+GovindSingh3011@users.noreply.github.com>
2025-03-14 14:32:00 +0530

    improve loading experience

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,17 @@
 import { Outlet } from "react-router-dom";
-import React, { useState, useEffect } from "react";
+import React from "react";
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from "./components/NavBar/NavBar";
 import Footer from "./components/Footer/Footer";
-import Preloader from "./components/PreLoader/PreLoader";
 // import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
-  const [load, upadateLoad] = useState(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      upadateLoad(false);
-    }, 1200);
-
-    return () => clearTimeout(timer);
-  }, []);
-
   return (
     <>
-      <Preloader load={load} />
-      <div className="App" id={load ? "no-scroll" : "scroll"}>
         <Navbar />
-        {/* <ScrollToTop /> */}
         <Outlet />
         <Footer />
-      </div>
     </>
   );
 }
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import homeLogo from "../../assets/home-main.svg";
 import Particle from "../Particle";
@@ -9,142 +9,159 @@ import { AiFillGithub } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 import "./Home.css";
+import Preloader from "../PreLoader/PreLoader";
 
 function Home() {
+  
+  const [load, upadateLoad] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      upadateLoad(false);
+    }, 1200);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <section>
-      <Container fluid className="home-section" id="home">
-        <Particle
-          options={{
-            particle: {
-              number: {
-                value: 50,
-              },
-              size: {
-                value: 3,
-              },
-            },
-          }}
-        />
-        <Particle />
+    <>
+      <Preloader load={load} />
+      <div className="App" id={load ? "no-scroll" : "scroll"}>
+        <section>
+          <Container fluid className="home-section" id="home">
+            <Particle
+              options={{
+                particle: {
+                  number: {
+                    value: 50,
+                  },
+                  size: {
+                    value: 3,
+                  },
+                },
+              }}
+            />
+            <Particle />
 
-        <Container className="home-content">
-          <Row>
-            <Col md={7} className="home-header">
-              <h1 style={{ paddingBottom: 15 }} className="heading">
-                Hi There!{" "}
-                <span className="wave" role="img" aria-labelledby="wave">
-                  👋🏻
-                </span>
-              </h1>
+            <Container className="home-content">
+              <Row>
+                <Col md={7} className="home-header">
+                  <h1 style={{ paddingBottom: 15 }} className="heading">
+                    Hi There!{" "}
+                    <span className="wave" role="img" aria-labelledby="wave">
+                      👋🏻
+                    </span>
+                  </h1>
 
-              <h1 className="heading-name">
-                I'M
-                <strong className="main-name"> GOVIND SINGH</strong>
-              </h1>
+                  <h1 className="heading-name">
+                    I'M
+                    <strong className="main-name"> GOVIND SINGH</strong>
+                  </h1>
 
-              <div style={{ padding: 50, textAlign: "left" }}>
-                <Type />
-              </div>
-            </Col>
+                  <div style={{ padding: 50, textAlign: "left" }}>
+                    <Type />
+                  </div>
+                </Col>
 
-            <Col md={5} style={{ paddingBottom: 20 }}>
-              <img
-                src={homeLogo}
-                alt="home pic"
-                className="img-fluid"
-                style={{ maxHeight: "450px" }}
-              />
-            </Col>
-          </Row>
-        </Container>
-      </Container>
+                <Col md={5} style={{ paddingBottom: 20 }}>
+                  <img
+                    src={homeLogo}
+                    alt="home pic"
+                    className="img-fluid"
+                    style={{ maxHeight: "450px" }}
+                  />
+                </Col>
+              </Row>
+            </Container>
+          </Container>
 
-      <Container fluid className="home-about-section" id="about">
-        <Container>
-          <Row>
-            <Col md={8} className="home-about-description">
-              <h1 style={{ fontSize: "2.6em" }}>
-                LET ME <span className="purple"> INTRODUCE </span> MYSELF
-              </h1>
-              <p className="home-about-body">
-                I fell in love with programming and I have at least learnt
-                something, <br />I think… 🤷‍♂️
-                <br />I am fluent in classics like
-                <i>
-                  <b className="purple"> Java, React. </b>
-                </i>
-                <br />
-                <br />
-                My field of Interest's are building new &nbsp;
-                <i>
-                  <b className="purple">Web Technologies and Products </b>
-                </i>
-                <br />
-                <br />
-                Whenever possible, I also apply my passion for developing
-                products with
-                <i>
-                  <b className="purple">
-                    {" "}
-                    Modern Javascript Library and Frameworks
-                  </b>
-                </i>
-                &nbsp; like
-                <i>
-                  <b className="purple"> React.js</b>
-                </i>
-              </p>
-            </Col>
-            <Col md={4} className="myAvtar">
-              <Tilt>
-                <img src={myImg} className="img-fluid" alt="avatar" />
-              </Tilt>
-            </Col>
-          </Row>
-          <Row>
-            <Col md={12} className="home-about-social">
-              <h1>FIND ME ON</h1>
-              <p>
-                Feel free to <span className="purple">connect </span>with me
-              </p>
-              <ul className="home-about-social-links">
-                <li className="social-icons">
-                  <a
-                    href="https://github.com/GovindSingh3011"
-                    target="_blank"
-                    rel="noreferrer"
-                    className="icon-colour  home-social-icons"
-                  >
-                    <AiFillGithub />
-                  </a>
-                </li>
-                <li className="social-icons">
-                  <a
-                    href="https://x.com/Govind_Singh30"
-                    target="_blank"
-                    rel="noreferrer"
-                    className="icon-colour  home-social-icons"
-                  >
-                    <FaXTwitter />
-                  </a>
-                </li>
-                <li className="social-icons">
-                  <a
-                    href="https://www.linkedin.com/in/govindsingh3011"
-                    target="_blank"
-                    rel="noreferrer"
-                    className="icon-colour  home-social-icons"
-                  >
-                    <FaLinkedinIn />
-                  </a>
-                </li>
-              </ul>
-            </Col>
-          </Row>
-        </Container>
-      </Container>
-    </section>
+          <Container fluid className="home-about-section" id="about">
+            <Container>
+              <Row>
+                <Col md={8} className="home-about-description">
+                  <h1 style={{ fontSize: "2.6em" }}>
+                    LET ME <span className="purple"> INTRODUCE </span> MYSELF
+                  </h1>
+                  <p className="home-about-body">
+                    I fell in love with programming and I have at least learnt
+                    something, <br />I think… 🤷‍♂️
+                    <br />I am fluent in classics like
+                    <i>
+                      <b className="purple"> Java, React. </b>
+                    </i>
+                    <br />
+                    <br />
+                    My field of Interest's are building new &nbsp;
+                    <i>
+                      <b className="purple">Web Technologies and Products </b>
+                    </i>
+                    <br />
+                    <br />
+                    Whenever possible, I also apply my passion for developing
+                    products with
+                    <i>
+                      <b className="purple">
+                        {" "}
+                        Modern Javascript Library and Frameworks
+                      </b>
+                    </i>
+                    &nbsp; like
+                    <i>
+                      <b className="purple"> React.js</b>
+                    </i>
+                  </p>
+                </Col>
+                <Col md={4} className="myAvtar">
+                  <Tilt>
+                    <img src={myImg} className="img-fluid" alt="avatar" />
+                  </Tilt>
+                </Col>
+              </Row>
+              <Row>
+                <Col md={12} className="home-about-social">
+                  <h1>FIND ME ON</h1>
+                  <p>
+                    Feel free to <span className="purple">connect </span>with me
+                  </p>
+                  <ul className="home-about-social-links">
+                    <li className="social-icons">
+                      <a
+                        href="https://github.com/GovindSingh3011"
+                        target="_blank"
+                        rel="noreferrer"
+                        className="icon-colour  home-social-icons"
+                      >
+                        <AiFillGithub />
+                      </a>
+                    </li>
+                    <li className="social-icons">
+                      <a
+                        href="https://x.com/Govind_Singh30"
+                        target="_blank"
+                        rel="noreferrer"
+                        className="icon-colour  home-social-icons"
+                      >
+                        <FaXTwitter />
+                      </a>
+                    </li>
+                    <li className="social-icons">
+                      <a
+                        href="https://www.linkedin.com/in/govindsingh3011"
+                        target="_blank"
+                        rel="noreferrer"
+                        className="icon-colour  home-social-icons"
+                      >
+                        <FaLinkedinIn />
+                      </a>
+                    </li>
+                  </ul>
+                </Col>
+              </Row>
+            </Container>
+          </Container>
+        </section>
+      </div>
+    </>
   );
 }
 
