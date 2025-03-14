73ad8f5de5687ef357d10bbabba2df059a810534
Govind Singh <110704907+GovindSingh3011@users.noreply.github.com>
2025-03-14 15:42:00 +0530

    Revert "add routing to improved navigation"
    This reverts commit d96c726f03e703215f678e0ff0958e08c1656266.

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,11 @@
+import { Outlet } from "react-router-dom";
 import React, { useState, useEffect } from "react";
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from "./components/NavBar/NavBar";
 import Footer from "./components/Footer/Footer";
 import Preloader from "./components/PreLoader/PreLoader";
-import Home from "./components/Home/Home";
-import About from "./components/About/About";
-import Projects from "./components/Project/Project";
-import Resume from "./components/Resume/Resume";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate,
-} from "react-router-dom";
-import ScrollToTop from "./components/ScrollToTop";
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+// import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   const [load, upadateLoad] = useState(true);
@@ -30,21 +19,15 @@ function App() {
   }, []);
 
   return (
-    <Router>
+    <>
       <Preloader load={load} />
       <div className="App" id={load ? "no-scroll" : "scroll"}>
         <Navbar />
-        <ScrollToTop />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/project" element={<Projects />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/resume" element={<Resume />} />
-          <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
+        {/* <ScrollToTop /> */}
+        <Outlet />
         <Footer />
       </div>
-    </Router>
+    </>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import {
+  Route,
+  RouterProvider,
+  Navigate,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
 import "./index.css";
-import App from "./App";
+import App from "./App.jsx";
+import Home from "./components/Home/Home.jsx";
+import Resume from "./components/Resume/Resume.jsx";
+import Contact from "./components/Contact/Contact.jsx";
+import Projects from "./components/Project/Project.jsx";
+import About from "./components/About/About.jsx";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="" element={<Home />} />
+      <Route path="/resume" element={<Resume />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/about" element={<About />} />
+      <Route path="*" element={<Navigate to="/" />} />
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
