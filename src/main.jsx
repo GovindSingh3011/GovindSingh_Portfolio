d96c726f03e703215f678e0ff0958e08c1656266
Govind Singh <110704907+GovindSingh3011@users.noreply.github.com>
2025-03-14 15:39:28 +0530

    add routing to improved navigation

--- src/main.jsx
+++ src/main.jsx
@@ -1,35 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {
-  Route,
-  RouterProvider,
-  Navigate,
-  createBrowserRouter,
-  createRoutesFromElements,
-} from "react-router-dom";
 import "./index.css";
-import App from "./App.jsx";
-import Home from "./components/Home/Home.jsx";
-import Resume from "./components/Resume/Resume.jsx";
-import Contact from "./components/Contact/Contact.jsx";
-import Projects from "./components/Project/Project.jsx";
-import About from "./components/About/About.jsx";
+import App from "./App";
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<App />}>
-      <Route path="" element={<Home />} />
-      <Route path="/resume" element={<Resume />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="/projects" element={<Projects />} />
-      <Route path="/about" element={<About />} />
-      <Route path="*" element={<Navigate to="/" />} />
-    </Route>
-  )
-);
-
-ReactDOM.createRoot(document.getElementById("root")).render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
