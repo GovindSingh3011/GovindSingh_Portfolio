8620c74a5e0e51e02b4acdc643e01ca1e389b08f
GovindSingh3011 <govindrawat3011@gmail.com>
2025-05-17 22:06:37 +0530

    Remove Home, Type, NavBar, Particle, PreLoader, Project, Resume components and associated styles; delete projects data and configuration files.

--- src/components/PreLoader/PreLoader.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./PreLoader.css";
-
-function PreLoader({ load }) {
-  return <div id={load ? "preloader" : "preloader-none"}></div>;
-}
-
-export default PreLoader;
